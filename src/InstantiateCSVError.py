c4df1b1123c85fa5fa18b38531c5d61f732f928b
miwnaya <mikgerasimov@gmail.com>
2023-06-27 08:34:40 +0300

    Fixed bug with exception message

--- src/InstantiateCSVError.py
+++ src/InstantiateCSVError.py
@@ -3,8 +3,14 @@ import csv, pathlib
 
 class FileNotFoundError(Exception):
     def __init__(self):
-        print('_Отсутствует файл item.csv_')
+        self.message = "_Отсутствует файл item.csv_"
+
+    def __str__(self):
+        return self.message
 
 class InstantiateCSVError(Exception):
     def __init__(self):
-        print('_Файл item.csv поврежден_')
+        self.message = "_Файл item.csv поврежден_"
+
+    def __str__(self):
+        return self.message
